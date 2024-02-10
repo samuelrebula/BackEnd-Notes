2b31aaa2dc1af7225731259cbee8dc212cc603d9
samuel <rebuuula@gmail.com>
2024-02-09 21:25:07 -0300

    criando as migrations

---
+++ src/database/migrations/20240209235641_user.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('user', function(table) {
+    table.string('user_id').primary().notNullable();
+    table.string('name').notNullable();
+    table.string('email').notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('user');
+};
